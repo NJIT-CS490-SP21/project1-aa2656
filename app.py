958a3c3e6028e9e76d9bc1164710f2497ee23235
EC2 Default User <ec2-user@ip-172-31-60-130.ec2.internal>
2021-02-05 03:55:49 +0000

    Initial commit. Created basic project structure. Created boilerplate flask app that displays temporary index.html.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import os
+import random
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    
+    return render_template(
+        'index.html',
+        )
+
+
+app.run(
+    port = 8080,
+    host=os.getenv('IP','0.0.0.0'),
+    debug=True
+)
