7f87cbf20e26ac397bbf9d725b9da6e65edcf61f
EC2 Default User <ec2-user@ip-172-31-60-130.ec2.internal>
2021-02-13 20:21:28 +0000

    Added genius.py

---
+++ genius.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+CLIENT_ACCESS = os.getenv('CLIENT_ACCESS_TOKEN')
+
+headers = {
+    'Authorization': f'Bearer {CLIENT_ACCESS}'
+}
+
+BASE_URL = 'https://api.genius.com'
+
+def get_lyrics(song_name):
+    r = requests.get(
+        BASE_URL+"/search",
+        headers=headers,
+        data={'q':song_name}
+    )
+    return r.json()['response']['hits'][0]['result']['url']
+    
+get_lyrics('god')
